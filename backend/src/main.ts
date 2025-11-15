7e3b80dd0b10b34b677a888a83f3611397976612
Artem Dmysh <dmshh@gmail.com>
2025-11-15 16:38:19 +0200

    feat: change one more env that was lost during merge

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     forbidNonWhitelisted: true,
     transform: true
   }))
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(ENV.PORT ?? 3000);
 }
 bootstrap();
