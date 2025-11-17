fa417b7c6d6b980024a7e0b80599052419f3707d
Artem Dmysh <dmshh@gmail.com>
2025-11-17 19:07:47 +0200

    feat: correct errors after merge conflict2

---
+++ backend/src/config/cloudinary.ts
@@ -0,0 +1,10 @@
+import {v2 as cloudinary} from 'cloudinary'
+import { ENV } from 'src/lib/env'
+
+
+cloudinary.config({
+    CLOUDINARY_API_KEY: ENV.CLOUDINARY_API_KEY as string,
+    CLOUDINARY_API_SECRET: ENV.CLOUDINARY_API_SECRET,
+    CLOUDINARY_NAME: ENV.CLOUDINARY_NAME
+})
+
